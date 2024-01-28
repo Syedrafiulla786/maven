bcfeef85d00fd6b62f5298cad5e0a7bdb6266c4d
syed <you@example.com>
2024-01-28 03:39:24 -0800

    pushes the changes for react js

---
+++ Maven-React/src/main/java/group2/config/PersistenceConfig.java
@@ -0,0 +1,15 @@
+
+package group2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class PersistenceConfig {
+
+    public PersistenceConfig() {
+        super();
+    }
+
+}
